0911403c325eaa7070972729edb8a1a2a438dfdd
aminediro <aminediro@pop-os.localdomain>
2022-11-07 19:17:15 +0100

    server reads from ring

---
+++ 5_iouring_gobwas/ring.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+
+	"github.com/gobwas/ws/wsutil"
+	"github.com/iceber/iouring-go"
+	"github.com/sirupsen/logrus"
+)
+
+const readSize int = 1024
+
+type WSRing struct {
+	iouring    *iouring.IOURing
+	resultChan chan iouring.Result
+	mu         *sync.RWMutex
+	conns      map[int]net.Conn
+}
+
+func MkRing(entries uint) (*WSRing, error) {
+	ring, err := iouring.New(entries)
+	if err != nil {
+		return nil, err
+	}
+	return &WSRing{
+		iouring:    ring,
+		resultChan: make(chan iouring.Result),
+		conns:      make(map[int]net.Conn),
+		mu:         &sync.RWMutex{},
+	}, nil
+}
+
+// Reads from the io_uring CQ
+func (wr *WSRing) Loop() {
+	log.Println("Starting CQ loop")
+	for {
+		result := <-wr.resultChan
+		switch result.Opcode() {
+
+		case iouring.OpRead:
+			wr.read(result)
+
+		case iouring.OpClose:
+			wr.close(result)
+		}
+	}
+}
+
+func (wr *WSRing) Add(conn net.Conn) error {
+
+	wr.mu.Lock()
+	defer wr.mu.Unlock()
+
+	fd := websocketFD(conn)
+
+	wr.conns[fd] = conn
+
+	buffer := make([]byte, readSize)
+	prep := iouring.Read(fd, buffer)
+	if _, err := wr.iouring.SubmitRequest(prep, wr.resultChan); err != nil {
+		log.Fatal("Ring add error", err)
+		return err
+	}
+	return nil
+}
+
+func (wr *WSRing) read(result iouring.Result) {
+	wr.mu.RLock()
+	defer wr.mu.RUnlock()
+
+	num := result.ReturnValue0().(int)
+	err := result.Err()
+
+	logrus.WithFields(logrus.Fields{
+		"id":          result.Fd(),
+		"Connection":  wr.conns[result.Fd()].RemoteAddr(),
+		"bytesToRead": result.ReturnValue0(),
+		"value1":      result.ReturnValue1(),
+	}).Info("Result")
+
+	if err != nil {
+		log.Fatal("Read error", err)
+	}
+
+	buffer, _ := result.GetRequestBuffer()
+	content := buffer[:num]
+
+	msg, _, err := wsutil.ReadClientData(buffer)
+	fmt.Printf("Content: %s\n", msg)
+
+	// This could be slow :
+	// Allocate buffer on each read !!
+	// TODO: Reuse the same buffer
+	// buffer := make([]byte, readSize)
+	prep := iouring.Read(result.Fd(), buffer)
+	if _, err := wr.iouring.SubmitRequest(prep, wr.resultChan); err != nil {
+		panic(err)
+	}
+}
+
+func (wr *WSRing) close(result iouring.Result) {
+	clientAddr := result.GetRequestInfo().(string)
+	if err := result.Err(); err != nil {
+		panic(err)
+	}
+	connPrintf(clientAddr, "close successful\n")
+}
+
+func websocketFD(conn net.Conn) int {
+	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
+	fdVal := tcpConn.FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+
+	return int(pfdVal.FieldByName("Sysfd").Int())
+}
+
+func connPrintf(addr string, format string, a ...interface{}) {
+	prefix := fmt.Sprintf("[%s]", addr)
+	fmt.Printf(prefix+format, a...)
+}
--- 5_iouring_gobwas/server.go
+++ 5_iouring_gobwas/server.go
@@ -1,133 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"reflect"
+	"log"
+	"net/http"
 	"syscall"
 
-	"github.com/iceber/iouring-go"
+	"github.com/gobwas/ws"
 )
 
-const readSize = 1024
+var ring *WSRing
 
-type WSRing struct {
-	ring     *iouring.IOURing
-	ringChan chan iouring.Result
-}
-
-func MkRing(entries uint) (*WSRing, error) {
-	ring, err := iouring.New(entries)
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection
+	conn, _, _, err := ws.UpgradeHTTP(r, w)
 	if err != nil {
-		return nil, err
+		return
 	}
-	return &WSRing{
-		ring:     ring,
-		ringChan: make(chan iouring.Result),
-	}, nil
-}
-func (wr *WSRing) Loop() {
-	for {
-		result := <-wr.ringChan
-		switch result.Opcode() {
-		case iouring.OpAccept:
-			// if _, err := iour.SubmitRequest(iouring.Accept(fd), resulter); err != nil {
-			// 	panicf("submit accept request error: %v", err)
-			// }
-			wr.accept(result)
-
-		case iouring.OpRead:
-			wr.read(result)
-
-		case iouring.OpWrite:
-			wr.write(result)
-
-		case iouring.OpClose:
-			wr.close(result)
-		}
+	if err := ring.Add(conn); err != nil {
+		log.Printf("Failed to add connection %v", err)
+		conn.Close()
 	}
 }
 
-func websocketFD(conn net.Conn) int {
-	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
-	fdVal := tcpConn.FieldByName("fd")
-	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
-
-	return int(pfdVal.FieldByName("Sysfd").Int())
-}
-
-func (wr *WSRing) read(result iouring.Result) {
-	clientAddr := result.GetRequestInfo().(string)
-	if err := result.Err(); err != nil {
-		panicf("[%s] read error: %v", clientAddr, err)
+func main() {
+	// Increase resources limitations
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
 	}
-
-	num := result.ReturnValue0().(int)
-	buf, _ := result.GetRequestBuffer()
-	content := buf[:num]
-
-	connPrintf(clientAddr, "read byte: %v\ncontent: %s\n", num, content)
-
-	buffer := make([]byte, readSize)
-	prep := iouring.Read(result.Fd(), buffer).WithInfo(clientAddr)
-	if _, err := iour.SubmitRequest(prep, resulter); err != nil {
-		panicf("submit read request error: %v", err)
-	}
-	// prep := iouring.Write(result.Fd(), content).WithInfo(clientAddr)
-	// if _, err := iour.SubmitRequest(prep, resulter); err != nil {
-	// 	panicf("[%s] submit write request error: %v", clientAddr, err)
-	// }
-}
-
-func (wr *WSRing) write(result iouring.Result) {
-	clientAddr := result.GetRequestInfo().(string)
-	if err := result.Err(); err != nil {
-		panicf("[%s] write error: %v", clientAddr, err)
-	}
-	connPrintf(clientAddr, "write successful\n")
-
-	// prep := iouring.Close(result.Fd()).WithInfo(clientAddr)
-	// if _, err := iour.SubmitRequest(prep, resulter); err != nil {
-	// 	panicf("[%s] submit write request error: %v", clientAddr, err)
-	// }
-}
-
-func close(result iouring.Result) {
-	clientAddr := result.GetRequestInfo().(string)
-	if err := result.Err(); err != nil {
-		panicf("[%s] close error: %v", clientAddr, err)
-	}
-	connPrintf(clientAddr, "close successful\n")
-}
-
-func listenSocket(addr string) int {
-	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
-	if err != nil {
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
 		panic(err)
 	}
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		panic(err)
+	// Creating Ring and starting
+	ring, _ = MkRing(1024)
+	go ring.Loop()
+
+	// Start WS Server
+	http.HandleFunc("/", wsHandler)
+	if err := http.ListenAndServe("0.0.0.0:8000", nil); err != nil {
+		log.Fatal(err)
 	}
-
-	sockaddr := &syscall.SockaddrInet4{Port: tcpAddr.Port}
-	copy(sockaddr.Addr[:], tcpAddr.IP.To4())
-	if err := syscall.Bind(fd, sockaddr); err != nil {
-		panic(err)
-	}
-
-	if err := syscall.Listen(fd, syscall.SOMAXCONN); err != nil {
-		panic(err)
-	}
-	return fd
-}
-
-func panicf(format string, a ...interface{}) {
-	panic(fmt.Sprintf(format, a...))
-}
-
-func connPrintf(addr string, format string, a ...interface{}) {
-	prefix := fmt.Sprintf("[%s]", addr)
-	fmt.Printf(prefix+format, a...)
 }
